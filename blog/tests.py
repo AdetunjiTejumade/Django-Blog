ffbc1892c195a0f8e7541930ff436b41dd1992f7
Adetunji Tejumade <tejumadeadetunji@gmail.com>
2020-08-17 12:58:53 +0100

    add tests

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
 
+from .models import Blog
 # Create your tests here.
+class BlogTests(TestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username='testuser',
+            email='test@email.com',
+            password='secret'
+        )
+
+        self.post = Blog.objects.create(
+            title='A good title',
+            body= 'A normal body',
+        )
+    def test_string_representation(self):
+        post = Blog(title='A good title')
+        self.assertEqual(str(post), post.title)
+
+
+    def test_get_absolute_url(self):
+        self.assertEqual(self.post.get_absolute_url(),'/1/')
